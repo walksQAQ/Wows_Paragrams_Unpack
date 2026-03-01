f17512a2dd67d58f13f25a354ad8823fa3beafd2
walks <2878449206@qq.com>
2026-03-02 01:40:35 +0800

    修复HullDefault无法正常读取的问题

--- ShipHullDataAnalyze.py
+++ ShipHullDataAnalyze.py
@@ -1,5 +1,3 @@
-import re
-
 class ShipHullDataAnalyze:
 
     # 数据解析/显示
@@ -29,31 +27,31 @@ class ShipHullDataAnalyze:
                 "label": "通用数据",
                 "items": {
                     "hull_id": {"name": "船体模块型号", "val": hull_id, "unit": "", "order": 0},
-                    "health": {"name": "基础血量", "val": hull_data.get("health"), "unit": "", "order": 1},
-                    "maxSpeed": {"name": "最大航速", "val": hull_data.get("maxSpeed"), "unit": "kts", "order": 2},
-                    "turningRadius": {"name": "转弯半径", "val": hull_data.get("turningRadius"), "unit": "m", "order": 3},
-                    "rudderTime": {"name": "转舵时间", "val": hull_data.get("rudderTime") * 0.77, "unit": "s", "order": 4},
-                    "vis_sea": {"name": "基础水面隐蔽", "val": hull_data.get("visibilityFactor"), "unit": "km", "order": 5},
-                    "vis_plane": {"name": "基础空中隐蔽", "val": hull_data.get("visibilityFactorByPlane"), "unit": "km", "order": 6},
+                    "health": {"name": "基础血量", "val": hull_data.get("health", 0), "unit": "", "order": 1},
+                    "maxSpeed": {"name": "最大航速", "val": hull_data.get("maxSpeed", 0), "unit": "kts", "order": 2},
+                    "turningRadius": {"name": "转弯半径", "val": hull_data.get("turningRadius", 0), "unit": "m", "order": 3},
+                    "rudderTime": {"name": "转舵时间", "val": hull_data.get("rudderTime", 0) * 0.77, "unit": "s", "order": 4},
+                    "vis_sea": {"name": "基础水面隐蔽", "val": hull_data.get("visibilityFactor", 0), "unit": "km", "order": 5},
+                    "vis_plane": {"name": "基础空中隐蔽", "val": hull_data.get("visibilityFactorByPlane", 0), "unit": "km", "order": 6},
                     "has_cit":{"name": "是否存在核心区模块", "val": has_cit, "unit": "", "order": 7},
-                    "hull_regenper":{"name": "船体回复率", "val": hull_data.get("Hull", {}).get("regeneratedHPPart"), "unit": "", "order": 8},
-                    "cit_regenper":{"name": "核心回复率", "val": hull_data.get("Cit", {}).get("regeneratedHPPart"), "unit": "", "order": 9},
+                    "hull_regenper":{"name": "船体回复率", "val": hull_data.get("Hull", {}).get("regeneratedHPPart", 0), "unit": "", "order": 8},
+                    "cit_regenper":{"name": "核心回复率", "val": hull_data.get("Cit", {}).get("regeneratedHPPart", 0), "unit": "", "order": 9},
                 }
             },
             "submarine_sp_data": {
                 "label": "潜艇特殊数据",
                 "items": {
                     "has_battery": {"name": "是否存在潜艇电力数据", "val": has_battery, "unit": "", "order": 0},
-                    "bat_cap": {"name": "电池容量", "val": sub_battery.get("capacity"), "unit": "", "order": 1},
-                    "bat_regen": {"name": "电力恢复", "val": sub_battery.get("regenRate"), "unit": "/s", "order": 2},
+                    "bat_cap": {"name": "电池容量", "val": sub_battery.get("capacity", 0), "unit": "", "order": 1},
+                    "bat_regen": {"name": "电力恢复", "val": sub_battery.get("regenRate", 0), "unit": "/s", "order": 2},
                     "has_hydrophone": {"name": "是否存在水听器模块数据", "val": has_hydrophone, "unit": "", "order": 3},
-                    "hp_radius": {"name": "水听器范围", "val": hydrophone.get("waveRadius"), "unit": "km", "order": 4},
-                    "hp_frep": {"name": "水听器更新周期", "val": hydrophone.get("updateFrequency"), "unit": "s", "order": 5},
-                    "hp_work_states": {"name": "水听器工作深度", "val": hydrophone.get("workingBuoyancyStates"), "unit": "", "order": 6},
-                    "hp_detect_states": {"name": "水听器可探测深度", "val": hydrophone.get("detectableBuoyancyStates"), "unit": "", "order": 7},
+                    "hp_radius": {"name": "水听器范围", "val": hydrophone.get("waveRadius", 0), "unit": "km", "order": 4},
+                    "hp_frep": {"name": "水听器更新周期", "val": hydrophone.get("updateFrequency", 0), "unit": "s", "order": 5},
+                    "hp_work_states": {"name": "水听器工作深度", "val": hydrophone.get("workingBuoyancyStates", 0), "unit": "", "order": 6},
+                    "hp_detect_states": {"name": "水听器可探测深度", "val": hydrophone.get("detectableBuoyancyStates", 0), "unit": "", "order": 7},
                     "buoyancyStates": {"name": "深度等级数据", "val": buoyancy_data, "unit": "", "order": 8},
-                    "buoyancy_rudder_time": {"name": "水平舵转舵时间", "val": hull_data.get("buoyancyRudderTime") * 0.77, "unit": "s", "order": 9},
-                    "buoyancy_speed": {"name": "上浮/下潜速度", "val": hull_data.get("maxBuoyancySpeed"), "unit": "m/s", "order": 10},
+                    "buoyancy_rudder_time": {"name": "水平舵转舵时间", "val": hull_data.get("buoyancyRudderTime", 0) * 0.77, "unit": "s", "order": 9},
+                    "buoyancy_speed": {"name": "上浮/下潜速度", "val": hull_data.get("maxBuoyancySpeed", 0), "unit": "m/s", "order": 10},
                 }
             }
         }
